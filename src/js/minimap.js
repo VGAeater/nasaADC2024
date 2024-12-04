31c6f65bb4945c3acdd55205aad3edd3316f1031
VGAeater <stratjel@gmail.com>
2024-12-03 22:51:12 -0500

    minimap map part (no tracker yet)

--- src/js/minimap.js
+++ src/js/minimap.js
@@ -1,6 +1,15 @@
 import * as c from "./constants.js";
 
 export const minimap = ( dataObject, shared ) => ( p ) => {
+	const orbitData = [
+		[[227, 139, 73], 0],			//EDL part 1
+		[[241, 64, 42], 115],			//orbiting earth
+		[[40, 169, 221], 1497],			// to the moon
+		[[255, 255, 255], 7093],		//back to earth
+		[[227, 139, 73], 12960]			//EDL part 2
+	];
+
+	// declare and set DOM object references
 	var main = document.getElementById('minimap');
 	var tab = document.getElementById('minimaptab');
 	var canvas = document.getElementById('minimapcanvas');
@@ -8,15 +17,68 @@ export const minimap = ( dataObject, shared ) => ( p ) => {
 
 	var offsetX, offsetY;
 
-	p.setup = () => {
-		p.createCanvas(320, 320, p.P2D, canvas);	// create the minimap canvas
-		p.background(255, 255, 0);		// clear background minimap canvas
+	var pgBase, pgBonus;
+
+	var doneDrawing = false;
+
+	function spaceToCanvas(x, y) {
+		let coeff = 320 / 427970.9797;
+		return [(x + 415970.9797) * coeff, (y + 242822.395) * coeff];
 	}
 
-	p.draw = () => {}
+	function drawPath(pInst, arr, selector, start=0, end=null) {
+		if (end == null)
+			end = arr[0].length-1;
+		for (let i = start; i < end; i++) {
+			let [x1, y1] = spaceToCanvas(arr[selector][i], arr[selector+2][i]);
+			let [x2, y2] = spaceToCanvas(arr[selector][i+1], arr[selector+2][i+1]);
+			pInst.line(x1, y1, x2, y2);
+		}
+	}
+
+	p.setup = () => {
+		p.createCanvas(320, 320, p.P2D, canvas);	// create the minimap canvas
+		pgBase = p.createGraphics(320, 320);	// pre graphic for base outline
+		pgBonus = p.createGraphics(320, 320);	// pre graphic for bonus bonoutline
+
+		pgBase.strokeWeight(4);
+		pgBonus.strokeWeight(4);
+
+		pgBase.background(0);
+		pgBonus.background(0);
+	}
+
+	p.draw = () => {
+		if (!(dataObject.baseReady && dataObject.bonusReady))	// stall until the data has been loaded (kinda hacky solution)
+			return;
+
+		if (!doneDrawing) {
+			pgBase.stroke(255, 255, 0);
+			pgBonus.stroke(255, 255, 0);
+
+			drawPath(pgBase, dataObject.bonusArr, c.arrayMoonStart);
+			drawPath(pgBonus, dataObject.bonusArr, c.arrayMoonStart);
+
+			for (let i = 0; i < orbitData.length; i++) {
+				pgBase.stroke(orbitData[i][0]);
+				drawPath(pgBase, dataObject.baseArr, c.arrayProbeStart, orbitData[i][1], orbitData[i+1] ? orbitData[i+1][1] : null);
+			}
+
+			for (let i = 0; i < orbitData.length; i++) {
+				pgBonus.stroke(orbitData[i][0]);
+				drawPath(pgBonus, dataObject.bonusArr, c.arrayProbeStart, orbitData[i][1], orbitData[i+1] ? orbitData[i+1][1] : null);
+			}
+			doneDrawing = true;
+		}
+
+		p.scale(1, -1);
+		p.image(pgBase, 0, -320);
+
+
+	}
 
 	tab.onmousedown = e => {
-		shared.isDragging = true;			// indicate that the minimap is being dragged
+		shared.isDragging = true;		// indicate that the minimap is being dragged
 		// set the original offset between the mouse and the top left corner of the minimap
 		offsetX = e.clientX - main.offsetLeft;
 		offsetY = e.clientY - main.offsetTop;

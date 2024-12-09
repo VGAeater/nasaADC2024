914337860afb2487d1dea75632ab8051000ea914
Pranav-P26 <pramod.pranav26@gmail.com>
2024-12-08 21:37:25 -0500

    Added React Native Comunication

--- src/js/main.js
+++ src/js/main.js
@@ -15,3 +15,25 @@ window.onload = () => {
 	sceneObject = new p5(scene(dataObject, shared));
 	minimapObject = new p5(minimap(dataObject, shared));
 }
+
+// Function to open the menu
+function openMenu() {
+	const menu = document.getElementById("menu");
+	if (menu) {
+	  menu.classList.toggle("hidden");
+	}
+   }
+   
+   
+   // Listen for messages from the WebView
+   document.addEventListener("message", (event) => {
+	console.log("Received message from React Native: " + event.data);
+	openMenu();
+   });
+   
+   
+   // For iOS WebView (since it uses `window.ReactNativeWebView` for communication)
+   window.addEventListener("message", (event) => {
+	console.log("Received message from React Native: " + event.data);
+	openMenu();
+   });
